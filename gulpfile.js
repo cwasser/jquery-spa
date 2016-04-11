c1196220b2ecbb22be408598407953b30e3dbf9a
cwasser <christian.wasser@trivago.com>
2016-04-11 19:30:38 +0200

    - updated gulpfile to use till now also browserify - some code cleanup - removed unused node modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,45 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var jshint = require('gulp-jshint');
-var sourcemaps = require('gulp-sourcemaps');
-var del = require('del');
-var map = require('map-stream');
-var qunit = require('node-qunit-phantomjs');
+'use strict';
 
-var DEST = './dist/';
-var PATHS = {
-    scripts: ['./src/**/*.js'],
-    test: './test/jquery.spa.html'
-};
+/**
+ * Import all plugins for gulp
+ */
+    // GULP
+var gulp = require('gulp'),
+    // GULP PLUGINS
+    // utility functions for gulp
+    gutil = require('gulp-util'),
+    // rename a file
+    rename = require('gulp-rename'),
+    // uglify js for minimizing
+    uglify = require('gulp-uglify'),
+    // concat for concat all js files
+    concat = require('gulp-concat'),
+    // sourcemaps out of the browserify
+    sourcemaps = require('gulp-sourcemaps'),
+    // jshint checker for gulp, coding style
+    jshint = require('gulp-jshint'),
+    // tap for using callbacks for each file in the pipe
+    tap = require('gulp-tap'),
+    // EXTERNAL PLUGINS
+    // delete something
+    del = require('del'),
+    map = require('map-stream'),
+    // making AMD modules ready for the browser
+    browserify = require('browserify'),
+    // vinyl-source-stream and buffer to use the stream of non gulp plugins
+    source = require('vinyl-source-stream'),
+    // reactify
+    reactify = require('reactify');
+
+
+var DEST = './dist/',
+    SRC_FILES = ['./src/spa/Data.js', './src/spa/History.js', './src/spa/Router.js', './src/spa/App.js','./src/spa.js'],
+    SRC_FILES_ALL = ['./src/**/*.js'],
+    BROWSERIFY_OPTS = {
+        entries : SRC_FILES,
+        debug : true
+    },
+    TARGET_FILENAME = 'jquery.spa.js';
 
 var logTime = function(){
     var date = new Date();
@@ -24,23 +50,33 @@ var logTime = function(){
     return '[' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2) + ']: ';
 };
 
-gulp.task('default',['clean','jshint','test','build'], function(){
+gulp.task('default',['clean','jshint','build'], function(){
     return gutil.log('clean and build successfully finished');
 });
 
-gulp.task('build', function(){
-   return gulp.src(PATHS.scripts)
-       .pipe(concat('jquery.spa.js'))
-       .pipe(sourcemaps.init())
-       .pipe(gulp.dest(DEST))
-       .pipe(uglify())
-       .pipe(rename({ extname: '.min.js' }))
-       .pipe(sourcemaps.write())
-       .pipe(gulp.dest(DEST));
+gulp.task('browserify', function(){
+    var bundler = browserify(BROWSERIFY_OPTS),
+        stream;
+    bundler.transform(reactify);
+    stream = bundler.bundle();
+
+    return stream.on('error', gutil.log.bind(gutil, logTime() + ' Browserify Error'))
+        .pipe(source(TARGET_FILENAME))
+        .pipe(gulp.dest(DEST));
+});
+
+gulp.task('build', ['browserify'],function(){
+    return gulp.src([DEST + TARGET_FILENAME])
+        .pipe(gulp.dest(DEST))
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(DEST));
 });
 
 gulp.task('jshint',function(){
-    return gulp.src(PATHS.scripts)
+    return gulp.src(SRC_FILES_ALL)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(map(function exitOnJsHintFail(file, cb){
@@ -51,16 +87,11 @@ gulp.task('jshint',function(){
         }));
 });
 
-gulp.task('test', function(){
-    //Implement a proper phantomjs testcase later on, for now: Deactivate tests
-    //qunit(PATHS.test, { 'verbose': true});
-});
-
 gulp.task('watch', function(){
     console.log(logTime() + 'Start watching JS sources and check them with jshint');
-    gulp.watch(PATHS.scripts, function(event){
+    gulp.watch(SRC_FILES_ALL, function(event){
         console.log(logTime() + 'File ' + event.path + ' was ' + event.type + ', checking with jshint...');
-        return gulp.src(PATHS.scripts)
+        return gulp.src(SRC_FILES_ALL)
             .pipe(jshint())
             .pipe(jshint.reporter('jshint-stylish'))
             .pipe(map(function showJsHintState(file, cb){
@@ -74,5 +105,5 @@ gulp.task('watch', function(){
 });
 
 gulp.task('clean', function(){
-    return del('dist/*');
+    return del([DEST + '*']);
 });
