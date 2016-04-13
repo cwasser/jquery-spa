120aa06687f02563161d4e09b72aedefd4b2c88a
cwasser <christian.wasser@trivago.com>
2016-04-13 15:27:43 +0200

    - add check if jquery exists - also predefined jQuery ($) in jshint

--- src/spa.js
+++ src/spa.js
@@ -5,34 +5,41 @@
  * Copyright (c) 2016 Christian Wasser
  * Licensed under the MIT license.
  */
-jQuery.spa = (function ( $ ) {
-    'use strict';
-    var App = require('./spa/App'),
-        Data = require('./spa/Data'),
-        History = require('./spa/History'),
-        Router = require('./spa/Router'),
-        testAll,
-        settings = {
-          App : App,
-          Data : Data,
-          History : History,
-          Router : Router
+(function ( $ ) {
+
+    // First check the hard dependencies for this plugin
+    if ( typeof $ !== "function" || $ === undefined ){
+        throw "jQuery.spa has a hard dependency on jQuery";
+    }
+
+    $.spa = (function ( $ ) {
+        var App = require('./spa/App'),
+            Data = require('./spa/Data'),
+            History = require('./spa/History'),
+            Router = require('./spa/Router'),
+            testAll,
+            settings = {
+                App : App,
+                Data : Data,
+                History : History,
+                Router : Router
+            };
+
+        testAll = function() {
+            window.console.log('RUNNING TEST ALL');
+            window.console.log('defined components are : ' + settings);
+            $.each(settings, function( component ){
+                component.call('test' + component );
+            });
         };
 
-  testAll = function() {
-    window.console.log('RUNNING TEST ALL');
-    window.console.log('defined components are : ' + settings);
-    $.each(settings, function( component ){
-      component.call('test' + component );
-    });
-  };
-
-  return {
-    testAll : testAll,
-    App : App,
-    Data : Data,
-    History : History,
-    Router : Router
-  };
-}( jQuery ));
+        return {
+            testAll : testAll,
+            App : App,
+            Data : Data,
+            History : History,
+            Router : Router
+        };
+    }( $ ));
+}( jQuery || $));
 
